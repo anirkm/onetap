f8a2f3726e9082d1c60b6e7dcf21dbbb494e7b33
Yatsuki <72000809+yatsukiuwu@users.noreply.github.com>
2023-01-04 18:20:21 +0100

    Add files via upload

---
+++ events/queue.js
@@ -0,0 +1,33 @@
+const { Queue, EVENTS } = require("queue-system");
+
+const createChannelQueue = new Queue();
+const deleteChannelQueue = new Queue();
+const testQueue = new Queue();
+
+function startTasks() {
+  createChannelQueue.on(EVENTS.TASK_ADD, (task) => {
+    console.log(`[Creation] ${task.data.user} created a onetap`);
+  });
+  deleteChannelQueue.on(EVENTS.TASK_ADD, (task) => {
+    console.log("[Deletion] a channel has been queued for deletion");
+  });
+
+  createChannelQueue.on(EVENTS.TASK_ERROR, (task, error) => {
+    console.log(`[Creation err] ${error}`);
+  });
+  deleteChannelQueue.on(EVENTS.TASK_ERROR, (task, error) => {
+    console.log(`[deletion err] ${error}`);
+  });
+}
+
+const testTask = () => {
+  console.log("test");
+};
+
+module.exports = {
+  createChannelQueue: createChannelQueue,
+  deleteChannelQueue: deleteChannelQueue,
+  testQueue: testQueue,
+  startTasks: startTasks,
+  testTask: testTask,
+};

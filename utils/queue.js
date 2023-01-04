43085bc5a59499c132d17aee9a3d5227e8ba44ce
Yatsuki <72000809+yatsukiuwu@users.noreply.github.com>
2023-01-04 18:25:32 +0100

    Add files via upload

--- utils/queue.js
+++ utils/queue.js
@@ -11,13 +11,6 @@ function startTasks() {
   deleteChannelQueue.on(EVENTS.TASK_ADD, (task) => {
     console.log("[Deletion] a channel has been queued for deletion");
   });
-
-  createChannelQueue.on(EVENTS.ERROR, (task, error) => {
-    console.log(`[Creation err] ${error}`);
-  });
-  deleteChannelQueue.on(EVENTS.TASK_ADD, (task, error) => {
-    console.log(`[deletion err] ${error}`);
-  });
 }
 
 const testTask = () => {
